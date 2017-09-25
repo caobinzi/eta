86b6ce08a6d150ef971278d24e46c122cce64e31
Binzi Cao <binzi.cao@bt.com>
2017-09-25 21:32:01 +1000

    one function should work

--- spark/src/Main.hs
+++ spark/src/Main.hs
@@ -7,22 +7,17 @@ import System.IO
 import System.Environment
 
 main :: IO ()
-main =  do
-  args <- getArgs
-  job $ args !! 0
- 
-job :: String -> IO ()
-job file = 
-        java $ do
+main = java $ do
+          args <- io getArgs
+          let file = args !! 0
           io $ putStrLn $ "Load file " ++ file
           conf <- newSparkConf
           conf <.> setAppName "Simple Application"
           sc <- newSparkContext conf
-          logData <- sc <.> textFile logfile >- cache
+          logData <- sc <.> textFile file >- cache
           numAs <- logData <.> S.filter (\(s :: String) -> 'a' `elem` s) >- count
           numBs <- logData <.> S.filter (\(s :: String) -> 'b' `elem` s) >- count
           io $ putStrLn $ "Lines with a: " ++ show numAs
                         ++ ", lines with b: " ++ show numBs
           sc <.> stop
-        where logfile = file
  
